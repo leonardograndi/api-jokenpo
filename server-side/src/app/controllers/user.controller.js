b2a43b0dc3d76599ed6d7ab43236372822e7a00f
Leonardo Grandi <leonardofelipegrandi@gmail.com>
2018-05-04 16:25:47 -0300

    Folders Structure

--- server-side/src/app/controllers/user.controller.js
+++ server-side/src/app/controllers/user.controller.js
@@ -1,6 +1,6 @@
-import User from '../models/user';
+import User from '../models/user.model';
 
-exports.findOne = async (req, res, next) => {
+export async function findOne(req, res, next) {
 
     const { email } = req.body;
 
@@ -8,16 +8,16 @@ exports.findOne = async (req, res, next) => {
         
         const user = await User.findOne({ email });
         
-        return res.send(200).send(user);
+        return res.status(200).send(user);
 
     } catch (error) {
         console.log(error);
-        return res.send(404).send({ message: 'User not found' });        
+        return res.status(404).send({ message: 'User not found' });        
     }
         
 };
 
-exports.createUser = async (req, res, next) => {
+export async function createUser(req, res, next) {
 
     const { nickname } = req.body;
 
@@ -29,7 +29,7 @@ exports.createUser = async (req, res, next) => {
 
     } catch (error) {
         console.log(error);
-        return res.send(404).send({ message: 'User not found' });        
+        return res.status(404).send({ message: 'User not found' });        
     }
         
 };
