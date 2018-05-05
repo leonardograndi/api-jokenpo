96367f3a08b9ba08555e621925b2e4940213c32f
Leonardo Grandi <leonardofelipegrandi@gmail.com>
2018-05-05 14:42:10 -0300

    ReactJS

---
+++ client-side/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    console.log("FF");
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h2>GOL</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
